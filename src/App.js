a0b7947e88dc381ed5e9fedb5b862629de590e2c
tawfik-dot <tawfikjbehi190@gmail.com>
2021-04-07 03:29:31 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>Hello world ! </h1>
+      </div>
+      <div className="container">
+      <div className="jumbotron">
+        <h1>titre </h1>
+        <p>go my code </p>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
